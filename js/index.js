cbb4b5972dd322f0eb7281d238f29636eef7ab82
jihad-hossain1 <jihadkhan934@gmail.com>
2023-03-21 17:04:40 +0600

    last change

--- js/index.js
+++ js/index.js
@@ -1,61 +1,78 @@
-const loadData = () =>{
-    fetch('https://restcountries.com/v3.1/all')
+const loadData = () => {
+  fetch('https://restcountries.com/v3.1/all')
     .then(res => res.json())
     .then(data => {
-        displayCountries(data)
+      displayCountries(data)
     })
 }
 
 
-const displayCountries = countries =>{
-    console.log(countries[0]);
-    const allCountriesHTML = countries.map(country => getCountryHtml(country));
-    // console.log(allCountriesHTML[0]);
-    const container = document.getElementById('display-item');
-    container.innerHTML = allCountriesHTML.join(' ');
+const displayCountries = countries => {
+  console.log(countries[0]);
+  const allCountriesHTML = countries.map(country => getCountryHtml(country));
+  // console.log(allCountriesHTML[0]);
+  const container = document.getElementById('display-item');
+  container.innerHTML = allCountriesHTML.join(' ');
 }
 
+//  option--> 1.  array of destruction
 
-const getCountryHtml = country =>{
-    return `
-        <div> 
-            <h2></h2>
-            <div class="card card-side bg-base-100 shadow-xl">
-        <figure>
-            <img src="${country.flags.png}" alt="Country"/>
-        </figure>
-        <div class="card-body">
-            <h2 class="card-title">${country.name.common}</h2>
-            <p>${country.capital ? country.capital : 'not avilable'}</p>
-            <div class="card-actions justify-end">
-            <button for="my-modal" id="my-modal" onclick="openModal()" class="btn btn-info btn-xs">see more</button>
-            </div>
-        </div>
-        </div>
-        </div>
+// const getCountryHtml = country => {
+//   const { flags,name } = country;
+//   return `
+//         <div class="card card-side bg-base-100 shadow-xl">
+//           <figure>
+//               <img src="${flags.png}" alt="Country"/>
+//           </figure>
+//           <div class="card-body">
+//               <h2 class="card-title">${name.common}</h2>
+//               <p>${country.capital ? country.capital : 'not avilable'}</p>
+//               <div class="card-actions justify-end">
+//               <button class="btn btn-info btn-xs">see more</button>
+//               </div>
+//           </div>
+//         </div>
+//     `;
+// }
 
-        
-    `
-}
+// option--> 2  array of destructuring by Parameter
 
-const openModal = () => {
-    console.log('ck');
-    const element = document.getElementById('my-modal')
-    element.innerHTML = `
-    <input type="checkbox" id="my-modal" class="modal-toggle" />
-      <div class="modal">
-        <div class="modal-box">
-          
-          <p class="py-4">
-            You've been selected for a chance to get one year of subscription to
-            use Wikipedia for free!
-          </p>
-          <div class="modal-action">
-            <label for="my-modal" class="btn">close</label>
+const getCountryHtml = ({ flags, name, capital }) => {
+
+  return `     
+        <div class="card card-side bg-base-100 shadow-xl">
+          <figure>
+              <img src="${flags.png}" alt="Country"/>
+          </figure>
+          <div class="card-body">
+              <h2 class="card-title">${name.common}</h2>
+              <p>${capital ? capital : 'not avilable'}</p>
+              <div class="card-actions justify-end">
+              <button class="btn btn-info btn-xs">see more</button>
+              </div>
           </div>
-        </div>
-      </div>
-    `
+        </div>   
+    `;
 }
+
+
+// const getCountryHtml = country => {
+
+//   return `     
+//         <div class="card card-side bg-base-100 shadow-xl">
+//           <figure>
+//               <img src="${country.flags.png}" alt="Country"/>
+//           </figure>
+//           <div class="card-body">
+//               <h2 class="card-title">${country.name.common}</h2>
+//               <p>${country.capital ? country.capital : 'not avilable'}</p>
+//               <div class="card-actions justify-end">
+//               <button class="btn btn-info btn-xs">see more</button>
+//               </div>
+//           </div>
+//         </div>   
+//     `;
+// }
+
 
 loadData();
