700fb1122d6975a9d89390b1983895a028d8b9f1
jihad-hossain1 <jihadkham934@gmail.com>
2023-03-21 13:34:17 +0600

    error in modal

--- js/index.js
+++ js/index.js
@@ -28,11 +28,33 @@ const getCountryHtml = country =>{
             <h2 class="card-title">${country.name.common}</h2>
             <p>${country.capital ? country.capital : 'not avilable'}</p>
             <div class="card-actions justify-end">
-            <button class="btn btn-info btn-xs">see more</button>
+            <button for="my-modal" id="my-modal" onclick="openModal()" class="btn btn-info btn-xs">see more</button>
             </div>
         </div>
         </div>
         </div>
+
+        
+    `
+}
+
+const openModal = () => {
+    console.log('ck');
+    const element = document.getElementById('my-modal')
+    element.innerHTML = `
+    <input type="checkbox" id="my-modal" class="modal-toggle" />
+      <div class="modal">
+        <div class="modal-box">
+          
+          <p class="py-4">
+            You've been selected for a chance to get one year of subscription to
+            use Wikipedia for free!
+          </p>
+          <div class="modal-action">
+            <label for="my-modal" class="btn">close</label>
+          </div>
+        </div>
+      </div>
     `
 }
 
